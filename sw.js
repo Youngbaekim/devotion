2e1305681a7cd8d4aa139446a788738ee1555d6e
Youngbaekim <102468617+Youngbaekim@users.noreply.github.com>
2026-04-04 21:34:57 +0900

    Add files via upload

---
+++ sw.js
@@ -0,0 +1,7 @@
+const CACHE = 'devotion-v1';
+self.addEventListener('install', e => {
+  e.waitUntil(caches.open(CACHE).then(c => c.addAll(['/devotion/'])));
+});
+self.addEventListener('fetch', e => {
+  e.respondWith(caches.match(e.request).then(r => r || fetch(e.request)));
+});
